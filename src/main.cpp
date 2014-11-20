4556beb2cec33fdc9e7ab76d886e8f82c97a1c0e
Kevin "Biomech" Putzier <kevinputzier01@gmail.com>
2014-11-20 12:02:42 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3271,10 +3271,14 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         return true;
     }
 
-
-
-
-
+if (pfrom->nVersion < 72000)
+    {
+      printf("partner %s using an old client %d, disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->nVersion);
+      pfrom->fDisconnect = true;
+      pfrom->fDisconnect = true;
+      return true;
+    }
+    
     if (strCommand == "version")
     {
         // Each connection can only send one version message
